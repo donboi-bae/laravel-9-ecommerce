3825ef2a8b525f3ebb7f9cc882dee5e35c72a521
Donboi <donaa1203@gmail.com>
2023-04-02 17:37:18 +0700

    first commit

---
+++ resources/views/create_product.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Product</title>
+</head>
+<body>
+    <form action="{{ route('store_product') }}" method="post" enctype="multipart/form-data">
+        @csrf
+        <input type="text" name="name" placeholder="Name">
+        <br>
+        <input type="text" name="description" placeholder="Description">
+        <br>
+        <input type="number" name="price" placeholder="Price">
+        <br>
+        <input type="number" name="stock" placeholder="Stock">
+        <br>
+        <input type="file" name="image">
+
+        <br>
+        <button type="submit">Submit Data</button>
+    </form>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/product/create', [ProductController::class, 'create_product'])->name('create_product');
+Route::post('/product/create', [ProductController::class, 'store_product'])->name('store_product');
+Route::get('/product', [ProductController::class, 'index_product'])->name('index_product');
+Route::get('/product/{product}', [ProductController::class, 'show_product'])->name('show_product');
+Route::get('/product/{product}/edit', [ProductController::class, 'edit_product'])->name('edit_product');
+Route::patch('/product/{product}/update', [ProductController::class, 'update_product'])->name('update_product');
+Route::delete('/product/{product}', [ProductController::class, 'delete_product'])->name('delete_product');
+
+Route::post('/cart/{product}', [CartController::class, 'add_to_cart'])->name('add_to_cart');
+Route::get('/cart', [CartController::class, 'show_cart'])->name('show_cart');
+Route::patch('/cart/{cart}', [CartController::class, 'update_cart'])->name('update_cart');
+Route::delete('/cart/{cart}', [CartController::class, 'delete_cart'])->name('delete_cart');
+
+Route::post('/checkout', [OrderController::class, 'checkout'])->name('checkout');
+Route::get('/order', [OrderController::class, 'index_order'])->name('index_order');
